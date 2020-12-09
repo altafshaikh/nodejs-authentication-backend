b1ec50afe8b37cb0cada924d98720af3141941ba
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-09 12:25:01 +0530

    linked contollers to the routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
+const { signUpUser, loginUser } = require("../controllers/userController");
 
 const userRoute = express.Router();
 
-userRoute.route("/signup").post();
-userRoute.route("/login").post();
+userRoute.route("/signup").post(signUpUser);
+userRoute.route("/login").post(loginUser);
 
 module.exports = userRoute;
