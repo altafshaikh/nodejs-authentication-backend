480f8445673991d063ae9808b9af38019ce2ff4b
altafshaikh <iamaltafshaikh07@gmail.com>
2020-12-09 12:18:03 +0530

    added user route inside the app

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const dontenv = require("dotenv");
 
-const blogRouter = require("./routes/blogRoutes");
+const userRouter = require("./routes/userRoutes");
 
 dontenv.config({ path: ".env" });
 const PORT = process.env.PORT;
 
 const app = express();
+
+app.use("/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Listeninig on Port http://localhost:${PORT}`);
